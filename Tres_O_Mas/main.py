71ecb5d748bc57cb2c46fff709b348e63be5f657
Christian <christian_ledesma_a@hotmail.com>
2022-05-13 22:19:34 +0200

    finished

--- Tres_O_Mas/main.py
+++ Tres_O_Mas/main.py
@@ -6,51 +6,85 @@ RONDAS = 10
 NUMERO_DADOS = 5
 
 
-def mostrar_puntuacion(jugadores):
+def mostrar_puntuaciones(jugadores):
     for jugador in jugadores:
         print(f"{jugador.nombre} tiene: {jugador.puntuacion_total} puntos")
 
 
-def sumar_puntos(jugador, puntos):
-    jugador.sumar_puntos(puntos)
+def comprobar_escalera(dados):
+    numero = 0
+    contador_escalera = 0
+    dados.sort()
+    for dado in dados:
+        if dado - numero == 1:
+            contador_escalera += 1
+        numero = dado
+    if contador_escalera == 5:
+        return True
+    return False
 
 
-def comprobar_puntos(jugador, ronda):
+def sumar_puntos(jugador, ronda):
     dados = jugador.dados[ronda]
     puntos = 0
+    numero_repetido = 0
+    escalera = comprobar_escalera(dados)
     for dado in dados:
-        iguales = list()
-        escalera = False
-        print(dado)
-    return puntos
+        vecesDado = dados.count(dado)
+        if vecesDado == 3:
+            if numero_repetido != dado:
+                puntos += 3
+            numero_repetido = dado
+        elif vecesDado == 4:
+            if numero_repetido != dado:
+                puntos += 6
+            numero_repetido = dado
+        elif vecesDado == 5:
+            if numero_repetido != dado:
+                puntos += 10
+            numero_repetido = dado
+    if escalera:
+        puntos += 5
+    jugador.sumar_puntos(puntos)
 
 
 def main():
     jugar = True
+    mensaje_jugar = "¿Desea jugar? Si/No: "
     while(jugar):
-        opcion_menu = input("¿Desea Jugar? Si/No: ").lower()
+        opcion_menu = input(mensaje_jugar).lower()
         if opcion_menu != "si":
             print("¡Adios!")
             jugar = False
             break
 
         print("Iniciando partida...")
-        jugador1 = Jugador("Usuario1")
-        jugador2 = Jugador("Usuario2")
+        jugador1 = Jugador("Máquina")
+        jugador2 = Jugador("Humano")
         for ronda in range(RONDAS):
             print(f"::::: Ronda: {ronda+1} :::::")
             dados_ronda_jugador1 = list()
             dados_ronda_jugador2 = list()
             for _ in range(NUMERO_DADOS):
                 dados_ronda_jugador1.append(Dado().tirar())
-                dados_ronda_jugador2.append(Dado().tirar())
-            jugador1.dados.append(dados_ronda_jugador1)
-            jugador2.dados.append(dados_ronda_jugador2)
+            jugador1.dados.append([dados_ronda_jugador1])
+            sumar_puntos(jugador1, ronda)
             print(f"{jugador1.nombre} ha sacado: {dados_ronda_jugador1}")
+
+            tirar_dados = input("¿Tirar dados? ").lower()
+            if tirar_dados == "si":
+                for _ in range(NUMERO_DADOS):
+                    dados_ronda_jugador2.append(Dado().tirar())
+                jugador2.dados.append(dados_ronda_jugador2)
+                sumar_puntos(jugador2, ronda)
+            else:
+                jugador2.dados.append([])
+
             print(f"{jugador2.nombre} ha sacado: {dados_ronda_jugador2}")
-            mostrar_puntuacion((jugador1, jugador2))
+            mostrar_puntuaciones((jugador1, jugador2))
             print("---------------------------------")
 
+        mensaje_jugar = "¿Desea volver a jugar? Si/No: "
 
 
 if __name__ == "__main__":
